79df0b525f9c4e6e815d85ed1729dd9d82e092f2
MarioReyes <cmario.reyesp@gmail.com>
2022-09-13 18:31:57 -0500

    First commit M2 Soy Henry

---
+++ demo.js
@@ -0,0 +1,17 @@
+const objeto = {
+    nombre: 'Carlos',
+    apellido: 'Reyes'
+};
+
+objeto.nombre = objeto.nombre + ' Mario';
+console.log(objeto.nombre);
+objeto.nombre;
+
+objeto.nombre = 'Marcela';
+console.log(objeto.nombre);
+
+objeto.celular = 3115556666;
+console.log(objeto.celular);
+
+console.log(objeto);
+
